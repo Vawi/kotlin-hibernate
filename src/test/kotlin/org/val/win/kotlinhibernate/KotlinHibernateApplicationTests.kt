11125ef826d35b56483b8362018d0f0c8122c646
Vawi <val.winnen@gmail.com>
2018-11-05 08:18:23 +0100

    Mise en place squelette  hibernate kotlin

---
+++ src/test/kotlin/org/val/win/kotlinhibernate/KotlinHibernateApplicationTests.kt
@@ -0,0 +1,16 @@
+package org.val.win.kotlinhibernate
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlinHibernateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
